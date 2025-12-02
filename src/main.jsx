8b75f2670f05b14f43436b28c66cd9aaa405ccbb
Eng-RAAF <salaad79@gmail.com>
2025-12-02 17:15:30 +0300

    first commit from cursor 188

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { ConvexProvider } from "./lib/convex.js";
-import { convex } from "./lib/convex.js";
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ConvexProvider client={convex}>
-      <App />
-      </ConvexProvider>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
 
--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,13 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
+    host: true,
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
-        changeOrigin: true
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path
       }
     }
   }
